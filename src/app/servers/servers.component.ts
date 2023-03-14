84da601b5bcfee1b910628e2c561b51d04afcd1e
Geng Sun <gsun@iastate.edu>
2023-03-14 16:41:19 -0500

    Done

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+ 
+  onCreateServer() {
+throw new Error('Method not implemented.');
+}
+
+
 
 }
