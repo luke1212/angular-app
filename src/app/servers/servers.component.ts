b10952b2f90cc83840db2690b3a9b5013ddb6bfc
Geng Sun <gsun@iastate.edu>
2023-03-16 22:08:35 -0500

    Added ChatGPT api

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { timestamp } from 'rxjs';
+import { Configuration, OpenAIApi } from "openai";
+import { environment } from "../../../environment";
 
 @Component({
   selector: 'app-servers',
@@ -15,29 +17,48 @@ export class ServersComponent {
   serverCreationStatus = 'No server was created!';
   serverName: string;
   userName = '';
+  questions = '';
+  gptOutput = '';
   isServerCreated = false;
   servers = ['Testserver', 'Testserver2'];
   isToggle = false;
   toggleDetails = [];
-  
-  constructor() { 
+  configuration: Configuration = new Configuration({
+    apiKey: environment.openAI.OPENAI_API_KEY,
+  });
+  openai: OpenAIApi = new OpenAIApi(this.configuration);
+  response: any;
+
+
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
- 
+
+  async onSubmit() {
+    this.response = await this.openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: this.questions,
+      temperature: 0,
+      max_tokens: 1000,
+    });
+    console.log(this.response);
+    this.gptOutput = this.response.data.choices[0].text;
+  }
+
   onCreateServer() {
     this.isServerCreated = true;
     this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is: ' + this.serverName + 'user name is: ' + this.userName;
   }
 
-  onToggleDetails() { 
+  onToggleDetails() {
     this.isToggle = true;
     this.toggleDetails.push(this.toggleDetails.length + 1);
   }
 
-  onChangeBackgroundColor() { 
+  onChangeBackgroundColor() {
     return this.toggleDetails.length >= 5 ? 'blue' : 'white';
   }
 }
